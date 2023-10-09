a96a570e6e9b04b947298482046a76e221017b8d
KOLANICH <kolan_n@mail.ru>
2023-10-09 21:49:21 +0300

    We have to move to Codeberg

--- hcd2lime/__init__.py
+++ hcd2lime/__init__.py
@@ -1,3 +1,7 @@
+from warnings import warn
+
+warn("We have moved from M$ GitHub to https://codeberg.org/KOLANICH-tools/hcd2lime.py , read why on https://codeberg.org/KOLANICH/Fuck-GuanTEEnomo .")
+
 def blobToWriteCommands(b, invalidCallback):
 	from .kaitai import parse
 
